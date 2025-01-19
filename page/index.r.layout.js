89491f991f1b0165f0a93542e538fff0933b1fed
Masudul Haque <saadixl@outlook.com>
2025-01-19 14:01:24 +0800

    Update design

--- page/index.r.layout.js
+++ page/index.r.layout.js
@@ -9,31 +9,31 @@ import {
 import { DEVICE_WIDTH } from "../utils/config/device";
 
 export const FETCH_BUTTON = {
-  x: (DEVICE_WIDTH - px(150)) / 2,
-  y: px(410),
-  w: px(150),
-  h: px(50),
+  x: (DEVICE_WIDTH - px(80)) / 2,
+  y: px(396),
+  w: px(80),
+  h: px(80),
   text_size: px(20),
-  radius: px(25),
+  radius: px(50),
   normal_color: 0x00bf63,
   press_color: DEFAULT_COLOR_TRANSPARENT,
   text: "Refresh",
 };
 
 export const APP_HEADER_TEXT = {
-  x: (DEVICE_WIDTH - px(150)) / 2,
-  y: px(30),
-  w: px(150),
+  x: (DEVICE_WIDTH - px(250)) / 2,
+  y: px(10),
+  w: px(250),
   h: px(200),
-  color: 0xdddddd,
-  text_size: px(20),
+  color: 0x777777,
+  text_size: px(30),
   align_h: hmUI.align.CENTER_H,
   align_v: hmUI.align.TOP,
   text_style: hmUI.text_style.WRAP,
 };
 
 export const FAJR_TEXT = {
-  x: px(-150),
+  x: px(-170),
   y: px(110),
   w: DEVICE_WIDTH - 2 * px(56),
   h: px(200),
@@ -45,7 +45,7 @@ export const FAJR_TEXT = {
 };
 
 export const SUNRISE_TEXT = {
-  x: px(260),
+  x: px(280),
   y: px(110),
   w: DEVICE_WIDTH - 2 * px(56),
   h: px(200),
@@ -57,7 +57,7 @@ export const SUNRISE_TEXT = {
 };
 
 export const DHUHR_TEXT = {
-  x: px(-150),
+  x: px(-170),
   y: px(210),
   w: DEVICE_WIDTH - 2 * px(56),
   h: px(200),
@@ -69,7 +69,7 @@ export const DHUHR_TEXT = {
 };
 
 export const ASR_TEXT = {
-  x: px(260),
+  x: px(280),
   y: px(210),
   w: DEVICE_WIDTH - 2 * px(56),
   h: px(200),
@@ -82,7 +82,7 @@ export const ASR_TEXT = {
 
 
 export const MAGHRIB_TEXT = {
-  x: px(-150),
+  x: px(-170),
   y: px(310),
   w: DEVICE_WIDTH - 2 * px(56),
   h: px(200),
@@ -95,7 +95,7 @@ export const MAGHRIB_TEXT = {
 
 
 export const ISHA_TEXT = {
-  x: px(260),
+  x: px(280),
   y: px(310),
   w: DEVICE_WIDTH - 2 * px(56),
   h: px(200),
