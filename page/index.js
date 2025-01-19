140581ca3bba6e8c6c6db082969d99137445631d
Masudul Haque <saadixl@outlook.com>
2025-01-19 15:59:03 +0800

    Refresh on select button click

--- page/index.js
+++ page/index.js
@@ -1,10 +1,9 @@
 import * as hmUI from "@zos/ui";
+import { onKey, KEY_SELECT, KEY_EVENT_CLICK } from '@zos/interaction';
 import moment from "moment";
 import { log as Logger } from "@zos/utils";
 import { BasePage } from "@zeppos/zml/base-page";
 import {
-  FETCH_BUTTON,
-  APP_HEADER_TEXT,
   FAJR_TEXT,
   SUNRISE_TEXT,
   DHUHR_TEXT,
@@ -12,41 +11,58 @@ import {
   MAGHRIB_TEXT,
   ISHA_TEXT,
   ACTIVE_TEXT,
+  CENTER_MESSAGE_TEXT,
 } from "zosLoader:./index.[pf].layout.js";
 
 const logger = Logger.getLogger("fetch_api");
+let centerMessageWidget, fajrWidget, sunriseWidget, dhuhrWidget, asrWidget, maghribWidget, ishaWidget;
 
 Page(
   BasePage({
     state: {},
     build() {
-      hmUI.createWidget(hmUI.widget.TEXT, {
-        ...APP_HEADER_TEXT,
-        text: `Singapore\n${moment().format('ddd MMM DD YYYY')}`,
-      });
-      hmUI.createWidget(hmUI.widget.BUTTON, {
-        ...FETCH_BUTTON,
-        click_func: (button_widget) => {
-          logger.log("click button");
-          this.fetchData();
+      onKey({
+        callback: (key, keyEvent) => {
+          if (key === KEY_SELECT && keyEvent === KEY_EVENT_CLICK) {
+            this.fetchData();
+          }
+          return true
         },
       });
+
       setTimeout(() => {
         this.fetchData();
       }, 10);
     },
     fetchData() {
+      hmUI.deleteWidget(centerMessageWidget);
+      hmUI.deleteWidget(fajrWidget);
+      hmUI.deleteWidget(sunriseWidget);
+      hmUI.deleteWidget(dhuhrWidget);
+      hmUI.deleteWidget(asrWidget);
+      hmUI.deleteWidget(maghribWidget);
+      hmUI.deleteWidget(ishaWidget);
+      centerMessageWidget = hmUI.createWidget(hmUI.widget.TEXT, {
+        ...CENTER_MESSAGE_TEXT,
+        text: "Loading prayer times...",
+      });
       this.request({
         method: "GET_DATA",
       })
         .then((data) => {
           logger.log("receive data");
+          hmUI.deleteWidget(centerMessageWidget);
+          centerMessageWidget = hmUI.createWidget(hmUI.widget.TEXT, {
+            ...CENTER_MESSAGE_TEXT,
+            text: `${moment().format('ddd MMM DD YYYY')}\nSingapore`,
+          });
           const { result = [] } = data;
           //const text = JSON.stringify(result);
           if (!result.length) {
-            hmUI.createWidget(hmUI.widget.TEXT, {
-              ...FAJR_TEXT,
-              text: "Please refresh to see prayer times.",
+            centerMessageWidget = hmUI.createWidget(hmUI.widget.TEXT, {
+              ...CENTER_MESSAGE_TEXT,
+              ...ERROR_COLOR,
+              text: "Something went wrong!",
             });
           } else {
             const isFajrNext = result[0].isUpcoming;
@@ -55,31 +71,31 @@ Page(
             const isMaghribNext = !isFajrNext && !isDhuhrNext && !isAsrNext && result[4].isUpcoming;
             const isIshaNext = !isFajrNext && !isDhuhrNext && !isAsrNext && !isMaghribNext && result[5].isUpcoming;
 
-            hmUI.createWidget(hmUI.widget.TEXT, {
+            fajrWidget = hmUI.createWidget(hmUI.widget.TEXT, {
               ...FAJR_TEXT,
               ...isFajrNext ? ACTIVE_TEXT : {},
               text: `${result[0].title}\n${result[0].time}`,
             });
-            hmUI.createWidget(hmUI.widget.TEXT, {
+            sunriseWidget = hmUI.createWidget(hmUI.widget.TEXT, {
               ...SUNRISE_TEXT,
               text: `${result[1].title}\n${result[1].time}`,
             });
-            hmUI.createWidget(hmUI.widget.TEXT, {
+            dhuhrWidget = hmUI.createWidget(hmUI.widget.TEXT, {
               ...DHUHR_TEXT,
               ...isDhuhrNext ? ACTIVE_TEXT : {},
               text: `${result[2].title}\n${result[2].time}`,
             });
-            hmUI.createWidget(hmUI.widget.TEXT, {
+            asrWidget = hmUI.createWidget(hmUI.widget.TEXT, {
               ...ASR_TEXT,
               ...isAsrNext ? ACTIVE_TEXT : {},
               text: `${result[3].title}\n${result[3].time}`,
             });
-            hmUI.createWidget(hmUI.widget.TEXT, {
+            maghribWidget = hmUI.createWidget(hmUI.widget.TEXT, {
               ...MAGHRIB_TEXT,
               ...isMaghribNext ? ACTIVE_TEXT : {},
               text: `${result[4].title}\n${result[4].time}`,
             });
-            hmUI.createWidget(hmUI.widget.TEXT, {
+            ishaWidget = hmUI.createWidget(hmUI.widget.TEXT, {
               ...ISHA_TEXT,
               ...isIshaNext ? ACTIVE_TEXT : {},
               text: `${result[5].title}\n${result[5].time}`,
--- page/index.r.layout.js
+++ page/index.r.layout.js
@@ -4,37 +4,40 @@ import { px } from "@zos/utils";
 import {
   ACTIVE_COLOR,
   DEFAULT_COLOR,
-  DEFAULT_COLOR_TRANSPARENT,
 } from "../utils/config/constants";
 import { DEVICE_WIDTH } from "../utils/config/device";
 
-export const FETCH_BUTTON = {
-  x: (DEVICE_WIDTH - px(80)) / 2,
-  y: px(396),
-  w: px(80),
-  h: px(80),
-  text_size: px(20),
-  radius: px(50),
-  normal_color: 0x00bf63,
-  press_color: DEFAULT_COLOR_TRANSPARENT,
-  text: "Refresh",
-};
-
-export const APP_HEADER_TEXT = {
+export const CENTER_MESSAGE_TEXT = {
   x: (DEVICE_WIDTH - px(250)) / 2,
-  y: px(10),
+  y: px(140),
   w: px(250),
   h: px(200),
-  color: 0x777777,
+  color: 0x0DBD2D,
+  text_size: px(25),
+  align_h: hmUI.align.CENTER_H,
+  align_v: hmUI.align.CENTER_V,
+  text_style: hmUI.text_style.WRAP,
+};
+
+export const ERROR_COLOR = {
+  color: 0xFF5733,
+};
+
+export const FAJR_TEXT = {
+  x: px(56),
+  y: px(2),
+  w: DEVICE_WIDTH - 2 * px(56),
+  h: px(200),
+  color: DEFAULT_COLOR,
   text_size: px(30),
   align_h: hmUI.align.CENTER_H,
   align_v: hmUI.align.TOP,
   text_style: hmUI.text_style.WRAP,
 };
 
-export const FAJR_TEXT = {
-  x: px(-170),
-  y: px(110),
+export const SUNRISE_TEXT = {
+  x: px(65),
+  y: px(100),
   w: DEVICE_WIDTH - 2 * px(56),
   h: px(200),
   color: DEFAULT_COLOR,
@@ -44,21 +47,9 @@ export const FAJR_TEXT = {
   text_style: hmUI.text_style.WRAP,
 };
 
-export const SUNRISE_TEXT = {
-  x: px(280),
-  y: px(110),
-  w: DEVICE_WIDTH - 2 * px(56),
-  h: px(200),
-  color: DEFAULT_COLOR,
-  text_size: px(30),
-  align_h: hmUI.align.LEFT,
-  align_v: hmUI.align.TOP,
-  text_style: hmUI.text_style.WRAP,
-};
-
 export const DHUHR_TEXT = {
-  x: px(-170),
-  y: px(210),
+  x: px(65),
+  y: px(280),
   w: DEVICE_WIDTH - 2 * px(56),
   h: px(200),
   color: DEFAULT_COLOR,
@@ -69,8 +60,21 @@ export const DHUHR_TEXT = {
 };
 
 export const ASR_TEXT = {
-  x: px(280),
-  y: px(210),
+  x: px(56),
+  y: px(270),
+  w: DEVICE_WIDTH - 2 * px(56),
+  h: px(200),
+  color: DEFAULT_COLOR,
+  text_size: px(30),
+  align_h: hmUI.align.CENTER_H,
+  align_v: hmUI.align.BOTTOM,
+  text_style: hmUI.text_style.WRAP,
+};
+
+
+export const MAGHRIB_TEXT = {
+  x: px(40),
+  y: px(260),
   w: DEVICE_WIDTH - 2 * px(56),
   h: px(200),
   color: DEFAULT_COLOR,
@@ -81,22 +85,9 @@ export const ASR_TEXT = {
 };
 
 
-export const MAGHRIB_TEXT = {
-  x: px(-170),
-  y: px(310),
-  w: DEVICE_WIDTH - 2 * px(56),
-  h: px(200),
-  color: DEFAULT_COLOR,
-  text_size: px(30),
-  align_h: hmUI.align.RIGHT,
-  align_v: hmUI.align.TOP,
-  text_style: hmUI.text_style.WRAP,
-};
-
-
 export const ISHA_TEXT = {
-  x: px(280),
-  y: px(310),
+  x: px(55),
+  y: px(90),
   w: DEVICE_WIDTH - 2 * px(56),
   h: px(200),
   color: DEFAULT_COLOR,
